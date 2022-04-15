aa38e193d5f6955f6c8a473b58d2410d69ef7597
DmityFilchev <filchev_dmitry@mail.ru>
2022-04-15 15:12:00 +0300

    'ok1'

--- Task13Vector3/Task13Vector3.cpp
+++ Task13Vector3/Task13Vector3.cpp
@@ -5,27 +5,20 @@ using namespace std;
 int main()
 {
 	int numbers = 0;
-	deque<int> num;
+	deque<int> num(5);
 	while(numbers!=-1)
 	{
-		if (num.size() <= 20)
-		{
-			cout << "Input numbers ";
-			cin >> numbers;
-			if (numbers != -1)
-				num.push_back(numbers);
-		}
-		if(num.size()>20)
-		{
-			num.erase(begin(num));
-			cout << "Input numbers ";
-			cin >> numbers;
-			if (numbers != -1)
-			num.push_back(numbers);
-		}
+		cout << "Input numbers ";
+		cin >> numbers;
+		if (numbers == -1)
+			break;
+		num.erase(begin(num));
+		num.push_back(numbers);
+
 	}
-	for (int i = 0; i <num.size(); i++)
-	{
-		cout << num.at(i) << " ";
+	for (auto i :num )
+	{	
+		if(i!=0)
+		cout << i << " ";
 	}
 }
